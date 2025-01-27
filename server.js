5c4ce8172bdd6e436e3aa03412e1feab05ef84e5
sanjay <sanjaybaidhya123@gmail.com>
2025-01-27 20:33:08 +0545

    domain change

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 // CORS Configuration
 app.use(
   cors({
-    origin: 'https://fir-8d81e.web.app', // Frontend domain
+    origin: 'https://cookie-p1yx.onrender.com/', // Frontend domain
     credentials: true, // Allow cookies
   })
 );
@@ -18,7 +18,7 @@ app.use(express.json()); // Enable parsing JSON in POST requests
 // Route to set the cookie
 app.post('/set-cookie', (req, res) => {
   res.cookie('myCookie', 'myValue', {
-    domain: 'fir-8d81e.web.app', // Target domain
+    domain: 'cookie-p1yx.onrender.com', // Target domain
     path: '/', // Cookie available site-wide
     secure: true, // HTTPS only
     httpOnly: true, // Not accessible via JavaScript
